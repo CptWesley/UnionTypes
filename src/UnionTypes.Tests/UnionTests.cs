5c63e956202cb1ee5c8f5b8395cd160e7b08f59c
Wesley Baartman <w.j.baartman@student.tudelft.nl>
2019-10-11 13:15:25 +0200

    Add overrides

--- src/UnionTypes.Tests/UnionTests.cs
+++ src/UnionTypes.Tests/UnionTests.cs
@@ -110,8 +110,73 @@ namespace UnionTypes.Tests
         [Fact]
         public void ExplicitCastNotAllowed()
         {
-            Union<int, string> union = new Union<int, string>(80);
+            Union<int, string> union = new Union<int, string>(32);
             AssertThat(() => { string str = (string)union; }).ThrowsExactlyException<InvalidCastException>();
+        }
+
+        /// <summary>
+        /// Checks that an object is considered equal to itself.
+        /// </summary>
+        [Fact]
+        public void EqualSame()
+        {
+            Union<int, string> union = new Union<int, string>(5463);
+            AssertThat(union).IsEqualTo(union).HasSameHashCodeAs(union);
+        }
+
+        /// <summary>
+        /// Checks that two identical objects are considered equal.
+        /// </summary>
+        [Fact]
+        public void EqualIdentical()
+        {
+            Union<int, string> union1 = new Union<int, string>(65);
+            Union<int, string> union2 = new Union<int, string>(65);
+            AssertThat(union1).IsEqualTo(union2).HasSameHashCodeAs(union2);
+        }
+
+        /// <summary>
+        /// Checks that two unions with different inner types are not considered equal.
+        /// </summary>
+        [Fact]
+        public void EqualDifferentType()
+        {
+            Union<int, string> union1 = new Union<int, string>(90);
+            Union<int, string> union2 = new Union<int, string>("dfsg");
+            AssertThat(union1).IsNotEqualTo(union2).DoesNotHaveSameHashCodeAs(union2);
+        }
+
+        /// <summary>
+        /// Checks that two unions with same inner types but different values are not considered equal.
+        /// </summary>
+        [Fact]
+        public void EqualDifferentValue()
+        {
+            Union<int, string> union1 = new Union<int, string>(1);
+            Union<int, string> union2 = new Union<int, string>(2);
+            AssertThat(union1).IsNotEqualTo(union2).DoesNotHaveSameHashCodeAs(union2);
+        }
+
+        /// <summary>
+        /// Checks that two unions with same inner types and both values being null are considered equal.
+        /// </summary>
+        [Fact]
+        public void EqualSameTypeBothNull()
+        {
+            Union<int[], string[]> union1 = new Union<int[], string[]>((int[])null);
+            Union<int[], string[]> union2 = new Union<int[], string[]>((int[])null);
+            AssertThat(union1).IsEqualTo(union2).HasSameHashCodeAs(union2);
+        }
+
+        /// <summary>
+        /// Checks that two unions with different inner types but same values are not considered equal.
+        /// </summary>
+        [Fact]
+        public void EqualDifferentTypeSameValue()
+        {
+            Union<int[], string[]> union1 = new Union<int[], string[]>((int[])null);
+            Union<int[], string[]> union2 = new Union<int[], string[]>((string[])null);
+            AssertThat(union1).IsNotEqualTo(union2).DoesNotHaveSameHashCodeAs(union2);
         }
     }
 }
